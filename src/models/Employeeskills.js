1f1b1576ef3bdd284b8732b4a178b2ac7f148e12
Zaid141997 <zaidpanhalkar03@gmail.com>
2023-06-13 20:05:29 +0530

    new changes

--- src/models/Employeeskills.js
+++ src/models/Employeeskills.js
@@ -1,46 +1,39 @@
-
 const mongoose = require('mongoose');
 const Skill = mongoose.model('Skill');
+
 const employeeSkillsSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
   },
+  
   primarySkill: {
-    skillId: {
-      type: mongoose.Schema.Types.ObjectId, // Update the type to reference the Skill model's ID
-      ref: 'Skill', // Reference the Skill model
-      required: true,
+    skill: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Skill', 
+      required: true
+     },
+    yearsOfExperience: { 
+      type: Number, 
+      required: true 
     },
-    yearsOfExperience: {
-      type: String,
-      required: true,
-    },
-    certification: {
-      type: String,
-    },
+    certification: { 
+      type: String 
+    }
   },
-  secondarySkills: [
-    {
-      skillId: {
-        type: mongoose.Schema.Types.ObjectId, // Update the type to reference the Skill model's ID
-        ref: 'Skill', // Reference the Skill model
-        required: true,
-      },
-      yearsOfExperience: {
-        type: String,
-        required: true,
-      },
-      certification: {
-        type: String,
-      },
-    },
-  ],
+  secondarySkills: [{
+    skill: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Skill',
+      required: true
+     },
+    yearsOfExperience: { 
+      type: Number
+     },
+    certification: {
+       type: String 
+      }
+  }]
 });
 
- mongoose.model('Employeeskills', employeeSkillsSchema);
-
-
-
-
-
+mongoose.model('Employeeskills', employeeSkillsSchema);
